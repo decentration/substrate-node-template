b63f641b7660f86bbc0e0e5a4b131300d94b1281
cuteolaf <oliverlim818@gmail.com>
2023-07-11 08:31:30 -0700

    update chain name

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -41,7 +41,7 @@ pub fn development_config() -> Result<ChainSpec, String> {
 
 	Ok(ChainSpec::from_genesis(
 		// Name
-		"Development",
+		"kabocha-parachain",
 		// ID
 		"dev",
 		ChainType::Development,
@@ -81,7 +81,7 @@ pub fn local_testnet_config() -> Result<ChainSpec, String> {
 
 	Ok(ChainSpec::from_genesis(
 		// Name
-		"Local Testnet",
+		"kabocha-parachain",
 		// ID
 		"local_testnet",
 		ChainType::Local,
